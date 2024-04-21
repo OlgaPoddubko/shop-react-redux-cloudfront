38f0f62bda5a6929d84b18128b7753fca3c29c14
olga-poddubko <olga.poddubko@xa.epicgames.com>
2024-04-21 23:02:03 +0200

    Update title

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Second Store!
+            GT Car Store
           </Link>
         </Typography>
 
