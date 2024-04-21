b5cd634ae3fafa9e0c2f31a779a20e5903fb0a1f
olga-poddubko <olga.poddubko@xa.epicgames.com>
2024-04-21 20:14:41 +0200

    update url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://rkivtstds1.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://pu1v4gtzmd.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
